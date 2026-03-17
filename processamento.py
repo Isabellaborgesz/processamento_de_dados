b0918c71538d37728052a1cca1075f60a0065d2d
ISABELLA BORGES FONSECA <47792521800@info.local>
2026-03-17 19:32:13 -0300

    arquivo txt

--- processamento.py
+++ processamento.py
@@ -21,4 +21,14 @@ def sistema(alunos):
             calculomedia = media
             melhor_aluno = nome
 
+
+    with open("resultado.txt", "w", encoding="utf-8") as arquivo:
+        arquivo.write("Alunos em recuperação:\n")
+        for nome, media in recuperacao:
+            arquivo.write(f"{nome}: {media:.2f}\n")
+
+        arquivo.write("\nMelhor aluno:\n")
+        arquivo.write(f"{melhor_aluno} com média {calculomedia:.2f}\n")
+
     return recuperacao, melhor_aluno, calculomedia
+
